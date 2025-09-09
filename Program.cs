860e981ac3b7626699f82aa7a2aaa0ccd546d50a
npvnpi <npvnpi@gmail.com>
2025-09-09 22:42:13 +0900

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿namespace CrazyArcadeServer
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var server = new AsyncServer();
+            server.Start(5000);
+
+            Console.ReadLine();
+        }
+    }
+}
